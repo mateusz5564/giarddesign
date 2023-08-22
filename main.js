64eb22297c7b4c0e2f32bf7bc9f99e6711bd12c0
Matt <37382981+mateusz5564@users.noreply.github.com>
2023-08-22 23:20:50 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,57 @@
+// menu dropdown (very basic)
+const navBtnOffer = document.querySelector("#nav-btn-offer");
+const navBtnOfferArrow = navBtnOffer.querySelector("img");
+const offerDropdown = document.querySelector("#offer-dropdown");
+
+navBtnOffer.addEventListener("click", e => {
+  if (offerDropdown.classList.contains("offer-dropdown-active")) {
+    navBtnOfferArrow.classList.remove("nav-btn-offer-arrow-active");
+    offerDropdown.classList.remove("offer-dropdown-active");
+  } else {
+    navBtnOfferArrow.classList.add("nav-btn-offer-arrow-active");
+    offerDropdown.classList.add("offer-dropdown-active");
+  }
+});
+
+// photos
+// masonry
+const photosContainer = document.querySelector(".photos");
+const macy = Macy({
+  container: photosContainer,
+  columns: 3,
+  trueOrder: true,
+  margin: { x: 42, y: 42 },
+});
+
+// expand photos
+const expandBtnPhotos = document.querySelector("#expand-btn-photos");
+const photosGradient = document.querySelector("#photos-gradient");
+
+expandBtnPhotos.addEventListener("click", () => {
+  photosContainer.classList.add("photos-container-expanded");
+  expandBtnPhotos.classList.add("hidden");
+  photosGradient.classList.add("hidden");
+});
+
+// fancybox
+Fancybox.bind("[data-fancybox]", {
+  // Your custom options
+});
+
+//swiper
+const swiper = new Swiper(".swiper", {
+  direction: "horizontal",
+  loop: true,
+  speed: 500,
+
+  autoplay: {
+    delay: 7000,
+    disableOnInteraction: false,
+    pauseOnMouseEnter: true,
+  },
+
+  navigation: {
+    nextEl: ".swiper-next",
+    prevEl: ".swiper-prev",
+  },
+});
